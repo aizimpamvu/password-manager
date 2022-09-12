651dbf077f728452b168204da80fbacfb7e51b60
aizimpamvu <hallyiizza200@gmail.com>
2022-09-12 14:19:35 +0200

    save to file

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from tkinter import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    with open("data.text", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -14,7 +24,7 @@ logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
-#Labels
+# Labels
 website_label = Label(text="Website")
 website_label.grid(row=1, column=0)
 email_label = Label(text="Email/Username")
@@ -22,7 +32,7 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password")
 password_label.grid(row=3, column=0)
 
-#Entries
+# Entries
 
 website_entry = Entry(width=60)
 website_entry.grid(row=1, column=1, columnspan=2)
@@ -33,14 +43,10 @@ email_entry.insert(0, "hallyizza200@gmail.com")
 password_entry = Entry(width=35)
 password_entry.grid(row=3, column=1)
 
-#Buttons
-generate_password_button = Button(text="Generate Password",width=20)
+# Buttons
+generate_password_button = Button(text="Generate Password", width=20)
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=50)
+add_button = Button(text="Add", width=50, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
-
-
-
-
 
 window.mainloop()
